1698d1730d820eccb692e2e293b3b51432279480
Muhammad Fadhlurrahman <fadhlifadhlurrahman18@gmail.com>
2024-09-24 23:21:54 +0700

    stuck bre, belom bikin button, show value sama ganti nama user
    pusing pusing

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// function validateForm(){
+//     const name = document.forms["message-form"]["name-input"].value;
+//     const birthDate = document.forms["message-form"]["birthDate"].value;
+//     const gender = document.forms["message-form"]["gender"].value;
+//     const messages = document.forms["message-form"]["messages"].value;
+
+//     if (name == "" || birthDate == "" || gender == "" || messages == ""){
+//         alert("tidak boleh kosong!");
+//         return false;
+//     }
+
+//     setSenderUI(name, birthDate, gender, messages); 
+    
+//     return false;
+// }
+
+// function setSenderUI(name, birthDate, gender, messages){
+//     document.getElementById("sender-name-input").innerHTML = name;
+//     document.getElementById("sender-birth-date").innerHTML = birthDate;
+//     document.getElementById("sender-gender").innerHTML = gender;
+//     document.getElementById("sender-messages").innerHTML = messages;
+// }
