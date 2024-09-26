1a3e33397092d647c85d88ecd0ddfaf8ef00f85c
Muhammad Fadhlurrahman <fadhlifadhlurrahman18@gmail.com>
2024-09-26 15:58:17 +0700

    JS BERHASILLLLL
    YANG DIPENGEN BERHASIL CUYYY

--- js/script.js
+++ js/script.js
@@ -1,32 +1,43 @@
 function validateForm(){
-    const name = document.forms["message-form"]["name-input"].value;
-    const birthDate = document.forms["message-form"]["birthDate"].value;
-    const gender = document.forms["message-form"]["gender"].value;
-    const messages = document.forms["message-form"]["messages"].value;
+    const name = document.forms["message-input"]["full-name"].value;
+    const birthDate = document.forms["message-input"]["birthDate"].value;
+    const gender = document.forms["message-input"]["gender"].value;
+    const messages = document.forms["message-input"]["messages"].value;
 
-    if (name == "" || birthDate == "" || gender == "" || messages == ""){
-        alert("tidak boleh kosong!");
+    if (name == ""){
+        alert("Nama tidak boleh kosong!");
+        return false;
+
+    } else if (birthDate == ""){
+        alert("Tanggal Lahir tidak boleh kosong!");
         return false;
     }
 
+    else if (gender == ""){
+        alert("pilih salah satu!");
+        return false;
+
+    }
+
+    else if (messages == ""){
+        alert("Pesan harap diisi!");
+        return false;
+
+    }
+    
     setSenderUI(name, birthDate, gender, messages); 
 
     return false;
 }
 
 function setSenderUI(name, birthDate, gender, messages){
-    document.getElementById("sender-name-input").innerHTML = name;
+    document.getElementById("sender-full-name").innerHTML = name;
     document.getElementById("sender-birth-date").innerHTML = birthDate;
     document.getElementById("sender-gender").innerHTML = gender;
     document.getElementById("sender-messages").innerHTML = messages;
-}
-
-function replaceName(){
-    var nameInput = prompt("masukkan nama anda", "")
-    document.getElementById("name").innerHTML = nameInput
+    document.getElementById("name-user").innerHTML = name;
 }
 
 document.getElementById("kirim").addEventListener("click", function(){
-    replaceName();
     validateForm();
 })
